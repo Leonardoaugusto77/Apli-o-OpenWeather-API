f37044ad736c33c1d8794d5480db75db4c5a6e95
Heisen <leonardopjh30@gmail.com>
2022-12-13 17:20:24 -0300

    updates

---
+++ js/javascript.js
@@ -0,0 +1,71 @@
+const APIKey = "97f3c5f19940d6df628ee91735c520e7"
+const apiCountryURL = 'https://countryflagsapi.com/png/'
+
+
+const cityInput = document.querySelector('#city-input')
+const searchBtn = document.querySelector('#search')
+
+const cityElement = document.querySelector('#city')
+const tempElement = document.querySelector('#temperature span')
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector('#weather-icon')
+const countryElement = document.querySelector('#country')
+const umidityElement = document.querySelector('#umidity span')
+const windElement = document.querySelector('#wind span')
+
+const weatherContainer = document.querySelector('#weather-data')
+
+
+
+// Functions
+
+
+const getshowWeatherData = async(city) => {
+    
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}&lang=pt_br`
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    return data
+}
+
+const showWeatherData = async (city) => {
+   const data =  await getshowWeatherData(city)
+
+   cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+  umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+
+  weatherContainer.classList.remove('hide')
+
+}
+
+
+
+// Events
+
+searchBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+
+    const city = cityInput.value
+
+    showWeatherData(city)
+   
+})
+
+cityInput.addEventListener('keyup', (e)=>{
+    if(e.code === 'Enter'){
+        const city = e.target.value
+
+        showWeatherData(city)
+    }
+})
